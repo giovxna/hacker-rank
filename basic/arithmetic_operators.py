0bb97514c34935246e7785169c41322b3cb46104
giovxna <gi.siqueira0202@gmail.com>
2024-06-14 15:32:09 -0300

    resolução de excs. básicos

---
+++ basic/arithmetic_operators.py
@@ -0,0 +1,13 @@
+
+a = int(input().strip())
+b = int(input().strip())
+
+soma = a + b
+
+diferenca = a - b
+
+produto = a * b
+
+print(soma)
+print(diferenca)
+print(produto)
---
+++ basic/if_else.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def check_weird(n):
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        if 2 <= n <= 5:
+            print("Not Weird")
+        elif 6 <= n <= 20:
+            print("Weird")
+        elif n > 20:
+            print("Not Weird")
+
+n = int(input().strip())
+
+check_weird(n)
